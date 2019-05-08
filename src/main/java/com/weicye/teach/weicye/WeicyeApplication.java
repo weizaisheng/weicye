9c776bde038d730117e6849ab975d2eec47ae237
weiwei <371964500@qq.com>
2019-05-08 17:47:32 +0800

    v1.0

---
+++ src/main/java/com/weicye/teach/weicye/WeicyeApplication.java
@@ -0,0 +1,13 @@
+package com.weicye.teach.weicye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeicyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeicyeApplication.class, args);
+	}
+
+}
